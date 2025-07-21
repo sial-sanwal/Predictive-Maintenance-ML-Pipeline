c8ab13da283deb3a17a832a9b9e8572a5d2cc298
sanwal-khan <sialsanwal885@gmail.com>
2025-07-22 03:10:21 +0500

    add the fastapi server

---
+++ app/app.py
@@ -0,0 +1,21 @@
+# app.py
+
+from fastapi import FastAPI, UploadFile, File
+import pandas as pd
+import joblib
+from src.preprocess import preprocess_data_for_prediction
+
+app = FastAPI()
+model = joblib.load("artifacts/model.pkl")
+
+@app.post("/predict")
+async def predict(file_telemetry: UploadFile = File(...), file_machines: UploadFile = File(...)):
+    telemetry = pd.read_csv(file_telemetry.file, parse_dates=["datetime"])
+    machines = pd.read_csv(file_machines.file)
+
+    df = preprocess_data_for_prediction(telemetry, machines)
+    input_data = df.drop(columns=["datetime"], errors="ignore")
+    preds = model.predict(input_data)
+
+    df["prediction"] = preds
+    return df[["machineID", "datetime", "prediction"]].to_dict(orient="records")
