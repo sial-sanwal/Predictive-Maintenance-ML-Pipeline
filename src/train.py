1a1ef0277bc0cd422d27351409c3dd9b22bfd0c9
sanwal-khan <sialsanwal885@gmail.com>
2025-07-22 11:58:47 +0500

    update

--- src/train.py
+++ src/train.py
@@ -14,7 +14,9 @@ def train_model():
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     model = build_model()
+    print("Model training Start>>>")
     model.fit(X_train, y_train)
+    
     joblib.dump(model, "model.pkl")
 
     y_pred = model.predict(X_test)
